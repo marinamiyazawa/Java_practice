e021c403735858e4db1de985b4ef619aca545b06
marinamiyazawa <mi.marina1030@gmail.com>
2020-08-27 18:37:33 +0900

    [finish]chapter03/04

---
+++ Java_practice/JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str1;
+		str1 = "MarinaMiyazawa";
+		System.out.println(str1);
+
+		str1 = "スイスイスイカ";
+		System.out.println(str1);
+		final int LUCKY_NUMBER = 2;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
