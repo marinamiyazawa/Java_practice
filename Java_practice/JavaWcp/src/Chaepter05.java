f9927eb16283b2c2ca522c382ae17e1c38cbf8af
marinamiyazawa <mi.marina1030@gmail.com>
2020-08-30 22:50:58 +0900

    [finish]Chapter05

---
+++ Java_practice/JavaWcp/src/Chaepter05.java
@@ -0,0 +1,21 @@
+
+public class Chaepter05 {
+	public static void main(String[] args) {
+		long long1 = 922222L;
+		System.out.println(long1);
+		double double1 = 3.14;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Integer int2 = 100;
+		System.out.println(int2);
+		Float float1 = 1.4142f;
+		System.out.println(float1);
+		Character char1 = 'A';
+		System.out.println(char1);
+
+		int[] array = {0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
